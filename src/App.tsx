cceb4b17d6c0943f5eaecf8e9dd61786b8ca08eb
Hamidreza <h.bigdeli2001@gmail.com>
2024-10-28 14:31:25 +0330

    feat(genres): create platform selector Author: Hamidreza

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenreList.tsx";
 import {Genre} from "./hooks/useGenres.ts";
 import {useState} from "react";
+import PlatformSelector from "./components/PlatformSelector.tsx";
 
 function App() {
     const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
@@ -19,6 +20,7 @@ function App() {
                     </GridItem>
                 </Show>
                 <GridItem area={'main'}>
+                    <PlatformSelector/>
                     <GameGrid selectedGenre={selectedGenre}/>
                 </GridItem>
             </Grid>
